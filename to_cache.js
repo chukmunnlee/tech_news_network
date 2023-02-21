9e6d74473d525602620e8b9af5f0c4630cd29170
chukmunnlee <chukmunnlee@gmail.com>
2023-02-22 07:30:37 +0800

    static files to cache

---
+++ to_cache.js
@@ -0,0 +1,18 @@
+const toCache = [
+	'/dov-bear.gif',
+	'/favicon.ico',
+	'/offline.html',
+	'/placeholder.png',
+	'/polar-bear.png',
+	'/styles.css',
+	'/unplugged.png',
+	'/manifest.json',
+	'/images/icon-128x128.png',
+	'/images/icon-144x144.png',
+	'/images/icon-152x152.png',
+	'/images/icon-192x192.png',
+	'/images/icon-384x384.png',
+	'/images/icon-512x512.png',
+	'/images/icon-72x72.png',
+	'/images/icon-96x96.png'
+]
